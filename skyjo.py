c72193dde33f130497ecec496d497cc913fdda24
Grinzold86 <linus.wohl@gmail.com>
2025-05-20 13:46:37 +0200

    Spieler erstellt

--- skyjo.py
+++ skyjo.py
@@ -7,3 +7,38 @@
 ##############################################################################
 # Beschreibung:
 ##############################################################################
+import player as Player
+import random
+# import pygame
+
+anzahlSpieler = 4   # Anzahl der Spieler / muss über den Host festgelegt werden
+
+skyjo_karten = (
+    *([-2] * 5),
+    *([-1] * 10),
+    *([0] * 15),
+    *([1] * 10),
+    *([2] * 10),
+    *([3] * 10),
+    *([4] * 10),
+    *([5] * 10),
+    *([6] * 10),
+    *([7] * 10),
+    *([8] * 10),
+    *([9] * 10),
+    *([10] * 10),
+    *([11] * 10),
+    *([12] * 10)
+)
+
+umgedrehteKarten = []
+aufgedeckteKarten = []
+
+spielerTupel = []
+for j in range(1, anzahlSpieler):
+    index = random.randint(0, len(skyjo_karten) - 1)
+    for i in range(1, 12):
+        umgedrehteKarten[i] = skyjo_karten[index]
+        skyjo_karten.pop(index)   # Karten ziehen
+    Spieler = Player(j, umgedrehteKarten)
+    spielerTupel.append(Spieler)    # Spieler-Objekte erstellen und in einer Liste speichern mit seinen Karten
