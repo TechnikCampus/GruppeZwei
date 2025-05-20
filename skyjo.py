bf06079f2a806e9e23a8f97e348666995b0e0bda
Tom <tomoh1304@gmail.com>
2025-05-20 11:33:42 +0200

    Update skyjo.py
    Habe mich mal selber hinzugefügt, nachdem ich ehrenlos vergessen wurde

--- skyjo.py
+++ skyjo.py
@@ -1,7 +1,7 @@
 ##############################################################################
 # Projekt:  Skyjo
 # Datei:    skyjo.py
-# Autor:    Linus Wohlgemuth (Grinzold86), Nico Leder (xNox33), Avinash Suthakaran (Avinash21-creator)
+# Autor:    Linus Wohlgemuth (Grinzold86), Nico Leder (xNox33), Avinash Suthakaran (Avinash21-creator), Tom Holst (tomholst)
 # Datum:    19.5.2025
 # Version:  1.0
 ##############################################################################
